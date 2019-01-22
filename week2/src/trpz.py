2ca87ece92366e7d0a282977b5688d423552a0ec
Zhi Yuan Lim <LimZY@Zhis-MacBook-Pro.local>
2019-01-22 11:05:36 +0800

    numPts is now a parameter

--- week2/src/trpz.py
+++ week2/src/trpz.py
@@ -6,18 +6,19 @@ def fnInt(x): ## This defines the function to be integrated.
     return x
 
 
-xMin = 0
-xMax = 1
-dx = 0.001
-numPts = (xMax - xMin)/dx
+xMin = 0.0
+xMax = 1.0
+numPts = 100
+dx = (xMax - xMin)/numPts
+
 X = linspace(xMin, xMax, numPts)
-Y = zeros(len(X))
+Y = zeros(numPts)
 result = 0
 
-for i in range(len(X)):
+for i in range(numPts):
     Y[i] = fnInt(X[i])
 
-for i in (range(len(X) - 1)):
+for i in range(numPts - 1):
     result += (Y[i+1] + Y[i]) * (dx / 2)
 
 print(result)
